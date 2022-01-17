a8da3542f8ee3b39d4d52fd244baf909f25407f4
Shynggys <chynga2002@gmail.com>
2022-01-17 06:38:11 +0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Шынгыс Курбан on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
